371275f4570265678e8a876f17b46f52febe836f
satyajit <satyajitdasmahapatra2002@gmail.com>
2026-04-14 13:16:22 +0530

    Implement contact detail page and add Filtering component

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,26 +6,23 @@ import { useContact } from "@/hooks/useContact";
 import { useEffect } from "react";
 
 import SearchBar from "@/components/ui/SearchBar";
+import { SkeletonCard } from "@/components/ui/SkeletonCard";
 function ContactPage() {
   const { getcontacts, loading, contacts, error } = useContact();
 
   useEffect(() => {
-    getcontacts({ page: 1, limit: 20 });
+    getcontacts({ page: 1, limit: 10 });
   }, []);
-
-
 
   return (
     <div className="p-3 xs:p-4 sm:p-5 md:p-6 lg:p-8 w-full min-h-screen bg-gray-50">
       <SearchBar />
-      {!loading && contacts.length === 0 ? (
-        <p className="text-gray-500 mt-6 text-sm sm:text-base">
-          No data found
-        </p>
+      {contacts.length === 0 ? (
+        <p className="text-gray-500 mt-6 text-sm sm:text-base">No data found</p>
       ) : (
         <div className="w-full min-h-screen  flex  flex-wrap gap-6 justify-start">
           {contacts.map((contact) => (
-            <ContactCard key={contact._id} contact={contact} />
+           loading ? <SkeletonCard key={contact._id} /> : <ContactCard key={contact._id} contact={contact} />
           ))}
         </div>
       )}
